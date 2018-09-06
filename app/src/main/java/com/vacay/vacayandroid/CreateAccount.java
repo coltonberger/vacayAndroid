61c8bf9d21d862b43c561e8eac563b77978a141f
Colton Berger <coltonberger@optonline.net>
2018-09-06 11:20:18 -0700

    added firebase to application

--- app/src/main/java/com/vacay/vacayandroid/CreateAccount.java
+++ app/src/main/java/com/vacay/vacayandroid/CreateAccount.java
@@ -3,44 +3,72 @@ package com.vacay.vacayandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class CreateAccount extends AppCompatActivity {
 
-    public TextView loginPage;
-    public TextView login;
+    //public TextView loginPage;
+    //public TextView login;
+
+    private EditText userFirstName, userLastName, userEmail, userPassword;
+    private TextView createAccountButton;
+    private TextView goBackButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        setupUIViews();
 
-        loginPage = findViewById(R.id.goBack);
-        loginPage.setOnClickListener(new View.OnClickListener() {
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openLoginPage();
+                if (validate()) {
+                    //upload info to database
+                }
             }
         });
 
-        login = findViewById(R.id.createAccount);
-        login.setOnClickListener(new View.OnClickListener() {
+        goBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openProfile();
+                startActivity(new Intent(CreateAccount.this, Login.class));
             }
         });
     }
 
-    public void openLoginPage() {
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
+    private void setupUIViews() {
+        userFirstName = (EditText) findViewById(R.id.firstName);
+        userLastName = (EditText) findViewById(R.id.lastName);
+        userEmail = (EditText) findViewById(R.id.email);
+        userPassword = (EditText) findViewById(R.id.password);
+        createAccountButton = (TextView) findViewById(R.id.createAccount);
+        goBackButton = (TextView) findViewById(R.id.goBack);
     }
 
-    public void openProfile() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    //validate if everything is filled out
+    private Boolean validate() {
+        Boolean result = false;
+
+        String firstName = userFirstName.getText().toString();
+        String lastName = userLastName.getText().toString();
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter all details", Toast.LENGTH_LONG).show();
+        } else {
+            result = true;
+        }
+
+        return result;
     }
+
 }
