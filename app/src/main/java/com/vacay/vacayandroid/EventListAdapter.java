98a2926a0fa51692359eb80c92c98b5a26742c10
Colton Berger <coltonberger@optonline.net>
2018-09-12 18:36:49 -0700

    able to add event to schedule

--- app/src/main/java/com/vacay/vacayandroid/EventListAdapter.java
+++ app/src/main/java/com/vacay/vacayandroid/EventListAdapter.java
@@ -16,9 +16,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -100,47 +105,79 @@ class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventListHo
 
                case R.id.add_to_schedule:
 
+                   /**
+                    *
+                    created_at
+                    :
+                    "2018-09-01T23:20:45.418Z"
+                    eventCity
+                    :
+                    "Los Angeles"
+                    eventDescription
+                    :
+                    "PLEASE JOIN US FOR A LITTLE COFFEE AND A LOT OF AMAZING AUTOMOBILES."
+                    eventImage
+                    :
+                    "http://ajeclassics.co.uk/wp-content/uploads/2016/03/Jag-129.jpg"
+                    eventName
+                    :
+                    "Cars and Coffee"
+                    eventPrice
+                    :
+                    5
+                    eventWebsite
+                    :
+                    "https://www.eventbrite.com/e/cars-and-coffee-la-tickets-34210770335"
+                    id
+                    :
+                    3
+                    updated_at
+                    :
+                    "2018-09-01T23:20:45.418Z"
+                    */
+
                    RequestQueue queue = Volley.newRequestQueue(v.getContext());
-                   Log.d("addToSchedule", "clicked id: "+ this.mSaveEvent.getEventId());
-                   Log.d("addToSchedule", "clicked name: "+ this.mSaveEvent.getEventName());
-                   Log.d("addToSchedule", "clicked city: "+ this.mSaveEvent.getEventCity());
-                   Log.d("addToSchedule", "clicked description: "+ this.mSaveEvent.getEventDescription());
+//                   Log.d("addToSchedule", "clicked id: "+ this.mSaveEvent.getEventId());
+//                   Log.d("addToSchedule", "clicked name: "+ this.mSaveEvent.getEventName());
+//                   Log.d("addToSchedule", "clicked city: "+ this.mSaveEvent.getEventCity());
+//                   Log.d("addToSchedule", "clicked description: "+ this.mSaveEvent.getEventDescription());
 
-                   String url = "https://vaca-backend.herokuapp.com/schedules/"+ this.mSaveEvent.getEventId();
-                   StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                           new Response.Listener<String>()
-                           {
-                               @Override
-                               public void onResponse(String response) {
-                                   Log.d("Response", response);
-                               }
-                           },
-                           new Response.ErrorListener()
-                           {
-                               @Override
-                               public void onErrorResponse(VolleyError error) {
-                                   Toast.makeText(v.getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                               }
-                           }
-                   ) {
+                   String url = "https://vaca-backend.herokuapp.com/schedules/";
+
+                   JSONObject parent = new JSONObject();
+                   JSONArray eventsList = new JSONArray();
+                   JSONObject event = new JSONObject();
+                   try {
+                       event.put("id", mSaveEvent.getEventId()+"");
+                       event.put("eventWebsite", mSaveEvent.getEventWebsite());
+                       event.put("eventPrice", mSaveEvent.getEventPrice() + "");
+                       event.put("eventName", mSaveEvent.getEventName());
+                       event.put("eventImage", mSaveEvent.getEventImage());
+                       event.put("eventDescription", mSaveEvent.getEventDescription());
+                       event.put("eventCity", mSaveEvent.getEventCity());
+
+                       eventsList.put(event);
+
+                       parent.put("events", eventsList);
+                       parent.put("userId", 1);
+
+                   } catch (JSONException e) {
+                       e.printStackTrace();
+                   }
+
+                   JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, parent, new Response.Listener<JSONObject>() {
                        @Override
-                       protected Map<String, String> getParams()
-                       {         // Adding parameters
-                           Map<String, String>  params = new HashMap<String, String>();
-                           params.put("id", mSaveEvent.getEventId());
-                           params.put("eventName", mSaveEvent.getEventName());
-                           params.put("eventCity", mSaveEvent.getEventCity());
-                           params.put("eventDescription", mSaveEvent.getEventDescription());
-                           params.put("schedule_Id", "1");
-
-                           Log.d("params", "getParams: "+ params);
-                           return params;
+                       public void onResponse(JSONObject response) {
+                            Toast.makeText(v.getContext(), "Event added", Toast.LENGTH_LONG).show();
                        }
-                   };
-                   queue.add(postRequest);
-//                   Intent addToScheduleIntent  = new Intent(v.getContext() , SavedSchedule.class);
-//                   prepareIntent(addToScheduleIntent);
-                   //Log.d("Clicked", "onClick: add to schedule clicked");
+                   }, new Response.ErrorListener() {
+                       @Override
+                       public void onErrorResponse(VolleyError error) {
+                           Toast.makeText(v.getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                       }
+                   });
+
+                    queue.add(request);
                    break;
 
            }
